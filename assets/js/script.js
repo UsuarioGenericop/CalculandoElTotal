5435946f70a8410f4e9eef4573020a44d35c7482
Andres <andydquintero@gmail.com>
2025-08-11 21:50:24 -0500

    AddedTotalCalculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ var result = document.querySelector(".number");
 
 // Set the text content
 result.innerHTML = 400000;
+var total = document.querySelector(".totalhtml");
+total.innerHTML = 0;
+
+// Function to calculate and display total price
+function calculateTotal() {
+  const precio = 400000; // Precio base
+  const cantidad = parseInt(document.querySelector(".totalhtml").innerHTML);
+  const totalPrecio = precio * cantidad;
+  document.querySelector(".number").innerHTML = totalPrecio;
+}
+
+// Initial calculation
+calculateTotal();
